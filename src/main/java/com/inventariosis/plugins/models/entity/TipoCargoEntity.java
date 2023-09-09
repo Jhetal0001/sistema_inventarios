5e163acb6d3222a259e979706cebc4994c889214
Jhetal0001 <jhetal00@gmail.com>
2023-09-08 20:52:34 -0500

    Se crea proyecto en Spring boot para sistema de inventario

---
+++ src/main/java/com/inventariosis/plugins/models/entity/TipoCargoEntity.java
@@ -0,0 +1,28 @@
+package com.inventariosis.plugins.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tipo_cargo")
+public class TipoCargoEntity implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3171114847067450283L;
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name = "nombre_cargo", nullable = false)
+	private String nombreCargo;
+}
